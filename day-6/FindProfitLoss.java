8840fc2d8d8bbdbfa1125eaa533f38e952e14539
Ankush Verma <ankushpandya488@gmail.com>
2025-02-03 21:35:04 +0530

    java

--- day-6/FindProfitLoss.java
+++ day-6/FindProfitLoss.java
@@ -1,27 +1,30 @@
 /*
   Write a java program to find the profit and loss by following the formula
-Profit and loss Formulas – 
-Profit = SP – CP
-Loss = CP – SP
+Profit and loss Formulas ï¿½ 
+Profit = SP ï¿½ CP
+Loss = CP ï¿½ SP
 Profit percentage = (Profit /Cost Price) x 100
 Loss percentage = (Loss / Cost price) x 100
 */
 
-public class  FindProfitLoss
-{
-	public static void main(String[] args) 
-	{
-		double sellingPrice = 1500;
-		double costPrice = 800;
+public class FindProfitLoss {
+    public static void main(String[] args) {
+        double sellingPrice = 7000;
+        double costPrice = 8900;
 
-		double profit = sellingPrice - costPrice;
-		double loss = costPrice - sellingPrice;
+        double profit = sellingPrice - costPrice;
+        double loss = costPrice - sellingPrice;
 
-		double profitPercentage = (profit / costPrice) * 100;
-        double lossPercentage = (loss/ costPrice) * 100;
-
-		System.out.println("Profit is :" + profitPercentage);
-		System.out.println("Loss is :" + lossPercentage);
-		
-	}
+        if (profit > 0) {
+            double profitPercentage = (profit / costPrice) * 100;
+            System.out.println("Profit is: " + profit);
+            System.out.println("Profit percentage is: " + profitPercentage + "%");
+        } else if (loss > 0) {
+            double lossPercentage = (loss / costPrice) * 100;
+            System.out.println("Loss is: " + loss);
+            System.out.println("Loss percentage is: " + lossPercentage + "%");
+        } else {
+            System.out.println("There is no profit or loss.");
+        }
+    }
 }
