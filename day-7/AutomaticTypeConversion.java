a6b217599cefbdd0544480799e65d8cd0079a2cb
Ankush Verma <ankushpandya488@gmail.com>
2025-02-04 19:14:49 +0530

    java

---
+++ day-7/AutomaticTypeConversion.java
@@ -0,0 +1,13 @@
+public class AutomaticTypeConversion
+{
+ public static void main (String args[])
+ {
+ int intVariable = 100;
+ long longVariable = intVariable;
+ float floatVariable = longVariable;
+
+ System.out.println ("Integer Value is : " + intVariable);
+ System.out.println ("Float Value is : " + floatVariable);
+ System.out.println ("Long Value is : " + longVariable);
+ }
+}
