6659f0e632501e1c5aa3305acb6bc0d090c7e93b
Ankush Verma <ankushpandya488@gmail.com>
2025-02-10 18:12:17 +0530

    java

--- day-11/PrintTable.java
+++ day-11/PrintTable.java
@@ -1,22 +1,40 @@
 // WAP in java to print table of the given number (Number must be taken from Scanner class)
 
+// import java.util.Scanner;
+
+// public class PrintTable 
+// {
+//     public static void main(String[] args) 
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a number to print table :");
+//         int num = sc.nextInt();
+// 		for(int j = 1 ;j<=10;j++)
+// 		{
+// 			 for(int i=1;i<=num;i++)
+// 			{
+// 				 System.out.print(j + " x " + i + " = " + (j * i)+"\t");
+// 			}
+// 			 System.out.println();
+// 		}
+		
+//     }
+// }
+
 import java.util.Scanner;
 
 public class PrintTable 
 {
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number to print table :");
-        int num = sc.nextInt();
-		for(int j = 1 ;j<=10;j++)
-		{
-			 for(int i=1;i<=num;i++)
-			{
-				 System.out.print(j + " x " + i + " = " + (j * i)+"\t");
-			}
-			 System.out.println();
-		}
-		
-    }
+   public static void main(String[] args) 
+   {
+	  Scanner sc = new Scanner(System.in);
+	  System.out.print("Enter a number to print Table :");
+	  int num = sc.nextInt();
+
+	  for(int i=1;i<=10;i++)
+	  {
+		System.out.println((num*i));
+	  }
+   }
+	
 }
